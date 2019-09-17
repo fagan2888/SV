33a81d3e5535578aa063a496c21723c0df8e9188
Michael Creel <michael.creel@uab.es>
2019-09-17 10:05:37 +0200

    add option to use log(y^2)

--- src/logL.jl
+++ src/logL.jl
@@ -1,11 +1,11 @@
 # asymptotic Gaussian likelihood function of statistic
-function logL(θ, m, n, shocks_u, shocks_e, withdet=true)
+function logL(θ, m, n, shocks_u, shocks_e, withdet=true, useabs=true)
     S = size(shocks_u,2)
     k = size(m,1)
     ms = zeros(eltype(aux_stat(SVmodel(θ, n, shocks_u[:,1], shocks_e[:,1]))), S, k)
     # this loop could be parallelized!
     Threads.@threads for s = 1:S
-        ms[s,:] = sqrt(n)*aux_stat(SVmodel(θ, n, shocks_u[:,s], shocks_e[:,s]))
+        ms[s,:] = sqrt(n)*aux_stat(SVmodel(θ, n, shocks_u[:,s], shocks_e[:,s]), useabs)
     end
     mbar = mean(ms,dims=1)[:]
     if ~any(isnan.(mbar))
