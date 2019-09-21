77f5e2a759e5bcc0dc8b9b2997c6d52640367325
Michael Creel <michael.creel@uab.es>
2019-09-21 16:38:09 +0200

    change parameterization

--- src/SVmodel.jl
+++ src/SVmodel.jl
@@ -8,16 +8,16 @@ end
 
 # the dgp: simple discrete time stochastic volatility (SV) model
 function SVmodel(θ, n, η, ϵ, savedata=false)
-    α = 2.0*log(θ[1])
+    ϕ = θ[1]
     ρ = θ[2]
     σ = θ[3]
     burnin = size(η,1) - n
     hlag = 0.0
     ys = zeros(n,1)
     for t = 1:burnin+n
-        h = α + ρ*(hlag - α) + σ*η[t] # figure out type
+        h = ρ*hlag + σ*η[t] # figure out type
         σt = exp(h/2.0)
-        y = σt*ϵ[t]
+        y = ϕ*σt*ϵ[t]
         if t > burnin 
             ys[t-burnin] = y
         end    
