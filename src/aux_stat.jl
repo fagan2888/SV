3c65cb03957c133db2040058e9b632cd88dfc183
Michael Creel <michael.creel@uab.es>
2019-11-08 11:34:53 +0100

    add unconditional moments

--- src/aux_stat.jl
+++ src/aux_stat.jl
@@ -1,4 +1,23 @@
 using Statistics
+function RawMoment(θ, m)
+    α = θ[1]
+    ρ = θ[2]
+    σ = θ[3]
+    exp(m*α/2.0 + (m^2.0)*σ/(1.0 - ρ^2.0)/4.0) # Shepphard GMM notes, page 384
+end
+
+function UnconditionalMoments(θ)
+    α = θ[1]
+    ρ = θ[2]
+    σ = θ[3]
+    vcat(
+        sqrt(2.0/pi)*RawMoment(θ,1),   # mean (abs(y))
+        RawMoment(θ,2),                # mean y^2
+        2.0*sqrt(2.0/pi)*RawMoment(θ,3), # mean abs(y^3))
+        3.0*RawMoment(θ,4)               # mean y^4
+        )
+end
+
 function aux_stat(y)
     α = sqrt(mean(y.^2.0))
     y = abs.(y)
@@ -12,5 +31,21 @@ function aux_stat(y)
         clusters = 1.0
     end
     # HAR model, for all params
-    vcat(α, clusters, HAR(y))
+    vcat(α, clusters, HAR(y), mean(abs.(y)), mean(y.^2.0), mean(abs.(y.^3.0)), mean(y.^4.0) )
+end
+
+function aux_stat(y, θ)
+    α = sqrt(mean(y.^2.0))
+    y = abs.(y)
+    # look for evidence of volatility clusters, for ρ
+    mm = ma(y,5)
+    mm = mm[5:end]
+    clusters = 0.0
+    try
+        clusters = quantile(mm,0.75)-quantile(mm, 0.25)
+    catch
+        clusters = 1.0
+    end
+    # HAR model, for all params
+    vcat(α, clusters, HAR(y), UnconditionalMoments(θ))
 end
