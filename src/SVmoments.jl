bd9f848fcc2aaaa319e3888f46cf9e4cdfb4377b
Michael Creel <michael.creel@uab.es>
2019-11-18 12:59:03 +0100

    fix

--- src/SVmoments.jl
+++ src/SVmoments.jl
@@ -1,6 +1,6 @@
 # for GMM 
 function SVmoments(m, n, θ, η, ϵ)
-    S = size(shocks_u, 2)
+    S = size(ϵ, 2)
     ms = zeros(S,size(m,1))
     Threads.@threads for s=1:S
         ms[s,:] = sqrt(n)*aux_stat(SVmodel(θ, n, η[:,s], ϵ[:,s])[1])
